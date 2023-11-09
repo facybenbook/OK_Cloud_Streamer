bed91184d4c51f71620ac4c9700dfbb5cf3f9b62
BattleAxeVR <belakampis@gmail.com>
2023-11-09 05:16:55 -0500

    Add config files

---
+++ config/deploy_cloudxr_client_settings.bat
@@ -0,0 +1,3 @@
+REM adb.exe push CloudXRLaunchOptions.txt /sdcard/CloudXRLaunchOptions.txt
+
+adb.exe push CloudXRLaunchOptions.txt /storage/emulated/0/Android/data/com.nvidia.cloudxr.ovr/files/CloudXRLaunchOptions.txt
---
+++ config/deploy_cloudxr_server_settings.bat
@@ -0,0 +1,1 @@
+copy CLoudXRServerOptions.txt %AppData%\..\Local\NVIDIA\CloudXR
